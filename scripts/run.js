51ce10d0b346f6b43837ebd950989922b7c7bac0
Pat Galvin <pgalv@amazon.com>
2022-04-20 13:46:13 -0400

    Push Files

---
+++ scripts/run.js
@@ -0,0 +1,50 @@
+const hre = require("hardhat");
+
+
+const main = async () => {
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+    const waveContract = await waveContractFactory.deploy({
+      value: hre.ethers.utils.parseEther("0.1"),
+    });
+    await waveContract.deployed();
+    console.log("Contract addy:", waveContract.address);
+  
+    let contractBalance = await hre.ethers.provider.getBalance(
+      waveContract.address
+    );
+    console.log(
+      "Contract balance:",
+      hre.ethers.utils.formatEther(contractBalance)
+    );
+  
+    /*
+     * Let's try two waves now
+     */
+
+    for (let i =0; i < 2; i++){
+        let waveTxn = await waveContract.wave("This is wave #"+i);
+        await waveTxn.wait();
+    };
+   
+  
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log(
+      "Contract balance:",
+      hre.ethers.utils.formatEther(contractBalance)
+    );
+  
+    // let allWaves = await waveContract.getAllWaves();
+    // console.log(allWaves);
+  };
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
